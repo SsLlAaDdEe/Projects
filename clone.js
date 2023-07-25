9c22386addb6d1cf010cf5e0aa8152163a32307b
Victory Arogundade <vickycom520@gmail.com>
2023-07-25 14:53:47 +0100

    first commit

---
+++ clone.js
@@ -0,0 +1,22 @@
+//This is my first JavaScript code
+console.log("Hello World");
+let name = "David";
+console.log(name);
+//Literals in JavaScript
+let word = 'Victory';//String Literal
+let age = 20;//Numeral Literal
+let isApproved = false;//Boolean literal
+let firstName = undefined;
+let selectedColour = null;
+
+//Object
+let person = {
+    name: 'Beth', age: 30
+};
+
+//Dot Notation
+person.age = 25
+
+//Bracket notation
+person['name'] = 'Mary'
+console.log(person)
